4c8a5dbe6f9e8019d346934e7c10b632c30c3997
cipow <cipoghost@gmail.com>
2017-11-12 12:46:37 +0700

    setting tablayout manager

--- app/src/main/java/com/example/cipowela/skos/MainActivity.java
+++ app/src/main/java/com/example/cipowela/skos/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.cipowela.skos;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.cipowela.skos.adapter.TabViewAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        pager.setAdapter(new TabViewAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(pager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
 }
