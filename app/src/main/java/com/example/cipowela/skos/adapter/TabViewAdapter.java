9503dd121fd58e956ee26cd369d0dde0b8ca3e43
cipow <cipoghost@gmail.com>
2017-11-12 12:43:26 +0700

    buat adapter tabview

---
+++ app/src/main/java/com/example/cipowela/skos/adapter/TabViewAdapter.java
@@ -0,0 +1,43 @@
+package com.example.cipowela.skos.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.cipowela.skos.fragment.menu.DaftarKosFragment;
+import com.example.cipowela.skos.fragment.menu.PemilikKosFragment;
+
+/**
+ * Created by cipowela on 12/11/17.
+ */
+
+public class TabViewAdapter extends FragmentPagerAdapter{
+
+    String[] data = {"Daftar Kos", "Pemilik Kos"};
+
+    public TabViewAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new DaftarKosFragment();
+            case 1:
+                return new PemilikKosFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return data.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return data[position];
+    }
+}
