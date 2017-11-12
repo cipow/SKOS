1becadc4837c1f11a13bf03799d7a5f17fc08e08
cipow <cipoghost@gmail.com>
2017-11-12 13:37:46 +0700

    setting menu ke fragment

--- app/src/main/java/com/example/cipowela/skos/fragment/menu/DaftarKosFragment.java
+++ app/src/main/java/com/example/cipowela/skos/fragment/menu/DaftarKosFragment.java
@@ -2,10 +2,15 @@ package com.example.cipowela.skos.fragment.menu;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.cipowela.skos.R;
 
@@ -27,4 +32,24 @@ public class DaftarKosFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_daftar_kos, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.filter_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.filter_daftar) {
+            Toast.makeText(getContext(), "filter klik", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/cipowela/skos/fragment/menu/PemilikKosFragment.java
+++ app/src/main/java/com/example/cipowela/skos/fragment/menu/PemilikKosFragment.java
@@ -2,10 +2,15 @@ package com.example.cipowela.skos.fragment.menu;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.cipowela.skos.R;
 
@@ -27,4 +32,29 @@ public class PemilikKosFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_pemilik_kos, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.pemilik_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.setting_menu) {
+            Toast.makeText(getContext(), "setting klik", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.logout_pemilik) {
+            Toast.makeText(getContext(), "logout klik", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
