2bcd429799a53e5c4b0f8445ec84b054f0bba8d9
cipow <cipoghost@gmail.com>
2017-11-12 13:08:58 +0700

    tambah logo ke action bar

--- app/src/main/java/com/example/cipowela/skos/MainActivity.java
+++ app/src/main/java/com/example/cipowela/skos/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         pager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
 
