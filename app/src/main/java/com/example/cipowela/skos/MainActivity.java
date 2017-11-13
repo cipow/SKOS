e1c407360e3c3643d79b3bbfcac8b78da88d768a
cipow <cipoghost@gmail.com>
2017-11-13 11:14:44 +0700

    edit main

--- app/src/main/java/com/example/cipowela/skos/MainActivity.java
+++ app/src/main/java/com/example/cipowela/skos/MainActivity.java
@@ -43,6 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+//        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                pager.getAdapter().notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
     }
 
 }
