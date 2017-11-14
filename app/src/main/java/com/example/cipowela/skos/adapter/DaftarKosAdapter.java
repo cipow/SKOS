994d4a024360316ea2fe2444ca618993346e49a3
cipow <cipoghost@gmail.com>
2017-11-14 11:23:06 +0700

    tambah detail kamar dan layouting

--- app/src/main/java/com/example/cipowela/skos/adapter/DaftarKosAdapter.java
+++ app/src/main/java/com/example/cipowela/skos/adapter/DaftarKosAdapter.java
@@ -1,6 +1,7 @@
 package com.example.cipowela.skos.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.cipowela.skos.DetailKosActivity;
 import com.example.cipowela.skos.R;
 import com.example.cipowela.skos.TestModel;
 
@@ -46,7 +48,7 @@ public class DaftarKosAdapter extends RecyclerView.Adapter<DaftarKosAdapter.DKAH
         holder.lihat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "anda lihat", Toast.LENGTH_SHORT).show();
+                context.startActivity(new Intent(context, DetailKosActivity.class));
             }
         });
     }
