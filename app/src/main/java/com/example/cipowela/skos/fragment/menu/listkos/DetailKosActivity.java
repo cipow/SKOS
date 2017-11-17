19b001f343e9b2293acd41852e005fd136ecec5d
cipow <cipoghost@gmail.com>
2017-11-18 01:34:14 +0700

    ubah warna title action bar

--- app/src/main/java/com/example/cipowela/skos/fragment/menu/listkos/DetailKosActivity.java
+++ app/src/main/java/com/example/cipowela/skos/fragment/menu/listkos/DetailKosActivity.java
@@ -1,7 +1,10 @@
 package com.example.cipowela.skos.fragment.menu.listkos;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 
 import com.example.cipowela.skos.R;
 
@@ -12,6 +15,12 @@ public class DetailKosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_kos);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String text = "S KOS";
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#FF80470A"))
+                , 0, text.length(), 0);
+        getSupportActionBar().setTitle(spannableString);
     }
 
     @Override
--- app/src/main/java/com/example/cipowela/skos/fragment/menu/ownerkos/DaftarActivity.java
+++ app/src/main/java/com/example/cipowela/skos/fragment/menu/ownerkos/DaftarActivity.java
@@ -1,7 +1,10 @@
 package com.example.cipowela.skos.fragment.menu.ownerkos;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 
 import com.example.cipowela.skos.R;
 
@@ -11,5 +14,18 @@ public class DaftarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String text = "Daftar S KOS";
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#FF80470A"))
+                , 0, text.length(), 0);
+        getSupportActionBar().setTitle(spannableString);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
