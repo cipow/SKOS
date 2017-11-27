a6dbcc3a0e577bc216649347ba01009299e65c2a
cipow <cipoghost@gmail.com>
2017-11-27 22:31:02 +0700

    detail dan edit kamar, masih kosong

---
+++ app/src/main/java/com/example/cipowela/skos/subactivity/KamarEditActivity.java
@@ -0,0 +1,15 @@
+package com.example.cipowela.skos.subactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cipowela.skos.R;
+
+public class KamarEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kamar_edit);
+    }
+}
